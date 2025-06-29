bc8595ae471a90d2347a673f69d1fa350be1c5a4
HomeEntwickler <169025881+HomeEntwickler@users.noreply.github.com>
2025-06-29 12:54:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ class ContentContainer(Container):
         self.expand = 4
         self.border_radius = 8
         self.bgcolor = Colors.GREEN_900
-
-        self.content = Text("Text von ContentViewContainer")
 #------------------------------------------------------------------------------------------------------------
 class ContentViewContainer(Container):
     def __init__(self, homePage) -> None:
@@ -20,6 +18,31 @@ class ContentViewContainer(Container):
 
         self.contentContainer = ContentContainer()
 
+        self.toolBar = Container(
+            bgcolor = "#17181d",
+            margin = margin.only(left = 0, top = -8, right = -8),
+            content = Row(
+                controls = [
+                    IconButton(
+                        icon = Icons.VIEW_SIDEBAR_OUTLINED, 
+                        icon_color = Colors.GREY_500,
+                        tooltip = "Anzeige der Seitenleiste auswählen",
+#                        on_click = self.sidebarAnimate,
+                    ),
+                    Container(expand = True),
+                    PopupMenuButton(
+                        icon = Icons.MORE_VERT,
+                        tooltip = "Mail Account konfigurieren",
+                        items = [
+                            PopupMenuItem(icon = Icons.MAIL_LOCK_OUTLINED, text = "Mail Account konfigurieren"), #on_click = lambda e: self.configureAccount(e)),
+                        ],
+                        opacity = 0.5,
+                    ), 
+                ]
+            )
+        )
+
+
         self.content = Row(
             expand = True,
             controls = [
@@ -27,8 +50,7 @@ class ContentViewContainer(Container):
                 Column(
                     expand = True,
                     controls = [
-                        Text("Toolbar Container"),
-#                        self.toolBar,
+                        self.toolBar,
                         self.contentContainer,
                     ]
                 ),
